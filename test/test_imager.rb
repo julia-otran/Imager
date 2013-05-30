da7d6a86786879b91114b24b8b81ce5c373bcedc
Guilherme <guilherme_otran@hotmail.com>
2013-05-30 17:50:54 -0300

    Delete tests

--- test/test_imager.rb
+++ test/test_imager.rb
@@ -1,7 +1,12 @@
 require 'test/unit'
 require 'imager'
 
+
 class ImagerTest < Test::Unit::TestCase
+  def your_base_uri;         "http://localhost/projects/imager_server"; end;
+  def your_manager_path;     "manager"; end;
+  def your_collections_path; "images"; end;
+  def your_auth_code;        ""; end;
 
   def test_configure
     Imager.configure do |c|
@@ -11,42 +16,42 @@ class ImagerTest < Test::Unit::TestCase
 
   def test_set_server_uri
     Imager.configure do |c|
-      c.base_uri = "http://localhost/projects/imager_server"
+      c.base_uri = your_base_uri
     end
 
-    assert_equal "http://localhost/projects/imager_server", Imager.base_uri
+    assert_equal your_base_uri, Imager.base_uri
   end
 
   def test_set_manager_path
     Imager.configure do |c|
-      c.manager_path = "manager"
+      c.manager_path = your_manager_path
     end
 
-    assert_equal "manager", Imager.manager_path
+    assert_equal your_manager_path, Imager.manager_path
   end
 
   def test_set_collection_path
     Imager.configure do |c|
-      c.collection_path = "images"
+      c.collection_path = your_collections_path
     end
 
-    assert_equal "images", Imager.collection_path
+    assert_equal your_collections_path, Imager.collection_path
   end
 
   def test_set_auth_code
     Imager.configure do |c|
-      c.auth_code = "123456789ABCDE"
+      c.auth_code = your_auth_code
     end
 
-    assert_equal "123456789ABCDE", Imager.auth_code
+    assert_equal your_auth_code, Imager.auth_code
   end
 
   def test_valid_post_image
     Imager.configure do |c|
-      c.base_uri = "http://localhost/projects/imager_server"
-      c.auth_code = ""
-      c.collection_path = "images"
-      c.manager_path = "manager"
+      c.base_uri = your_base_uri
+      c.auth_code = your_auth_code
+      c.collection_path = your_collections_path
+      c.manager_path = your_manager_path
     end
 
     response = Imager::ServerInterface.post("testcollection", "1", "test/image.jpg", small: { width: 100})
@@ -57,12 +62,40 @@ class ImagerTest < Test::Unit::TestCase
     response = true
     begin
       Imager.configure do |c|
-        c.base_uri = "http://localhost/projects/imager_server"
-        c.auth_code = ""
-        c.collection_path = "images"
-        c.manager_path = "manager"
+        c.base_uri = your_base_uri
+        c.auth_code = your_auth_code
+        c.collection_path = your_collections_path
+        c.manager_path = your_manager_path
       end
       Imager::ServerInterface.post("", "", File.new("test/test_image.jpg"), small: { width: 100})
+    rescue
+      response = false
+    end
+    assert_equal false, response
+  end
+
+  def test_valid_delete_image
+    Imager.configure do |c|
+      c.base_uri = your_base_uri
+      c.auth_code = your_auth_code
+      c.collection_path = your_collections_path
+      c.manager_path = your_manager_path
+    end
+
+    response = Imager::ServerInterface.delete("testcollection", "1", "image")
+    assert_equal true, response
+  end
+
+  def test_invalid_delete_image
+    response = true
+    begin
+      Imager.configure do |c|
+        c.base_uri = your_base_uri
+        c.auth_code = your_auth_code
+        c.collection_path = your_collections_path
+        c.manager_path = your_manager_path
+      end
+      Imager::ServerInterface.delete("testcollection", "1", "non_existing_image")
     rescue
       response = false
     end
